55c1c2402bea92d44534ab762b65d806167f1644
Iliah Nazarov <48030498+meta110@users.noreply.github.com>
2020-05-23 22:08:35 +0300

    Update fias.py

--- geo/fias.py
+++ geo/fias.py
@@ -4,6 +4,7 @@ Created on Sat May 23 02:21:29 2020
 
 Loads all actual FIAS data from ADDROB*.dbf files to PostgreSQL database 
 Actual version of files is always here: https://fias.nalog.ru/Updates
+More info about FIAS https://ru.wikipedia.org/wiki/%D0%A4%D0%B5%D0%B4%D0%B5%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F_%D0%B8%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D0%B0%D1%8F_%D0%B0%D0%B4%D1%80%D0%B5%D1%81%D0%BD%D0%B0%D1%8F_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0
 
 @author: meta110
 """
